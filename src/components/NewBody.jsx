ee079e87eadc98ae5800f6c1ed536f00159f5035
Molo Munyansanga <molomunyansanga@gmail.com>
2025-08-04 19:48:21 +0200

    Deleted files unrelated to project. They were used for learning purposes

--- src/components/NewBody.jsx
+++
@@ -1,5 +0,0 @@
-export default function NewBody(props) {
-    return (
-        <h1 className="p-3 text-2xl font-semibold">Welcome back, {props.username}!</h1>
-    )
-}
--- src/components/count.jsx
+++
@@ -1,6 +0,0 @@
-export default function Count(props) {
-    return (
-        <h2 className="count flex items-center justify-center w-25 h-25 bg-white text-2xl rounded-full">{props.number}</h2>
-
-    )
-}
