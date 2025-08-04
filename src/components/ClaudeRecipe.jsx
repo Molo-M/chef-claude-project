faf0fd8aad20a1b695be9555118e2cbca5f86cd7
Molo Munyansanga <molomunyansanga@gmail.com>
2025-08-04 19:52:42 +0200

    Changed the width of recipe for better desktop viewing

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -3,10 +3,10 @@ import ReactMarkdown from 'react-markdown'
 
 export default function ClaudeRecipe(props) {
     return (
-        <div className="markdown_container">
+        <section className="markdown_container">
             <h1 className="font-semibold text-2xl">Chef Claude Recommends:</h1>
             <ReactMarkdown>{props.AI_respone}</ReactMarkdown>
-        </div>
+        </section>
         
     )
 }
